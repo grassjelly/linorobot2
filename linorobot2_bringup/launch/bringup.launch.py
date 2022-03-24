72e73e6df500c8825d581829d3ed8997c21e8f3d
Juan Miguel Jimeno <jimenojmm@gmail.com>
2022-03-24 14:55:19 +0700

    changed to udev rule serial port

--- linorobot2_bringup/launch/bringup.launch.py
+++ linorobot2_bringup/launch/bringup.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             name='base_serial_port', 
-            default_value='/dev/ttyACM1',
+            default_value='/dev/linobase',
             description='Linorobot Base Serial Port'
         ),
 
